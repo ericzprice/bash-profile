d01bf7b5017b694d6554b9441e01e24cac5baf59
ericzprice <ericzprice@gmail.com>
2017-08-06 20:39:02 -0600

    new repository

---
+++ main.js
@@ -0,0 +1,15 @@
+/*
+
+Eric Price's cusotmized Bash Profile
+
+alias ga='git add'
+alias gaa='git add .'
+alias gaaa='git add --all'
+alias gau='git add --update'
+alias gb='git branch'
+alias gbd='git branch --delete '
+alias gc='git commit'
+alias gcm='git cgommit --message'
+alias gcf='git commit --fixup'
+alias gco='git checkout'
+alias gcob='git checkout -b'
